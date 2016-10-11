c7c79035b5f8f7e4fe9f92e6429d27daf11f0d12
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-10-11 16:23:31 -0400

    error handling

---
+++ snippets.py
@@ -0,0 +1,25 @@
+import logging
+
+logging.basicConfig(filename="snippets.log", level= logging.DEBUG)
+
+def put(name, snippet):
+    """
+    Store a snippt with an associated name.
+
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+
+def get(name):
+    """
+    Retrieve the snippet with a given name.
+
+    If there is no such snippet, return '404: Snippet Not Found'.
+
+    Returns the snippet.
+    """
+
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+
